460496528a18f6a9ffdc33138a8d5f1f90940ddd
vikash chand <vikash.chand2020@vitstudent.ac.in>
2023-10-02 04:09:01 +0530

    initial push

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter as Router
+
+import Navbar1 from "./Components/Navigationbar/Navbar";
+import Cart from "./Screens/Cart/Cart";
+import { LandingPage } from "./Screens/LandingPage/LandingPage";
+import Productspage from "./Screens/Productspage/Productspage";
+
+
+
+export default function App() {
+  return (
+    <Router>
+      <div className="main-container">
+        <Navbar1 />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/products" element={<Productspage/> } />
+          {/* Add a catch-all route */}
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </div>
+    </Router>
+
+  );
+}
---
+++ src/Screens/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './segments/Header'
+import ShopByCategory from './segments/ShopByCategory'
+import BasicStarterPack from './segments/BasicStarterPack'
+
+
+export const LandingPage = () => {
+  return (
+    <>
+      <Header/>
+      <ShopByCategory/>
+      <BasicStarterPack/>  
+
+    </>
+  )
+}
